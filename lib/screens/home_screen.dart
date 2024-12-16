128bc42e590d68989949deb69fdb7cac4b0d15d3
Somye <mahajan.somye@gmail.com>
2024-12-16 18:01:31 +0530

    Added provider & ui change

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/screens/taskform_screen.dart';
+import 'package:taskly/widgets/theme_mode_switch.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Taskly'),
+        actions: const [ThemeModeSwitch()],
       ),
       body: const Center(
         child: Text('Welcome to Taskly!'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Add task functionality
           Navigator.push(
             context,
             MaterialPageRoute(
