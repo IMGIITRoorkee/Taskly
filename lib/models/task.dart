a09a999759c744c1ca9d87c7072f72d9b61ff822
sanat <sanatjha4@gmail.com>
2024-12-16 18:20:33 +0530

    fixed add task option #16 and saving locally #12

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -4,4 +4,22 @@ class Task {
   bool isCompleted;
 
   Task({required this.title, this.description = '', this.isCompleted = false});
+
+  // Convert a Task object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  // Create a Task object from JSON
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      title: json['title'],
+      description: json['description'],
+      isCompleted: json['isCompleted'],
+    );
+  }
 }
--- lib/screens/tasklist_screen.dart
+++ lib/screens/tasklist_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import '../models/task.dart';
+import 'package:taskly/models/task.dart';
 
 class TaskListScreen extends StatelessWidget {
   final List<Task> tasks;
+  final Function(int, bool?) onToggle;
 
-  const TaskListScreen({super.key, required this.tasks});
+  const TaskListScreen({super.key, required this.tasks, required this.onToggle});
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +14,16 @@ class TaskListScreen extends StatelessWidget {
       itemBuilder: (context, index) {
         final task = tasks[index];
         return ListTile(
-          title: Text(task.title),
+          title: Text(
+            task.title,
+            style: TextStyle(
+              decoration: task.isCompleted ? TextDecoration.lineThrough : null,
+            ),
+          ),
           subtitle: Text(task.description),
           trailing: Checkbox(
             value: task.isCompleted,
-            onChanged: (value) {
-              // Toggle task completion
-            },
+            onChanged: (value) => onToggle(index, value),
           ),
         );
       },
