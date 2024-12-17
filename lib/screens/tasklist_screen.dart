fb95fed7aab680a1ad1fc14d297904f313c59237
sanat <sanatjha4@gmail.com>
2024-12-17 11:07:54 +0530

    Added slidable option to delete a task

--- lib/screens/tasklist_screen.dart
+++ lib/screens/tasklist_screen.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/models/task.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:taskly/task_storage.dart';
 
-class TaskListScreen extends StatelessWidget {
+class TaskListScreen extends StatefulWidget {
   final List<Task> tasks;
   final Function(int, bool?) onToggle;
 
   const TaskListScreen({super.key, required this.tasks, required this.onToggle});
 
   @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
       itemBuilder: (context, index) {
-        final task = tasks[index];
-        return ListTile(
+        final task = widget.tasks[index];
+        return Slidable(
+          endActionPane: ActionPane(motion: StretchMotion(), children: [
+            SlidableAction(
+              onPressed: (context) async {
+                setState(() {
+                widget.tasks.removeAt(index);
+                });
+                await TaskStorage.saveTasks(widget.tasks);
+
+              },
+              icon: Icons.delete,
+              foregroundColor: Colors.red,
+            ),
+          ]),
+          child: ListTile(
           title: Text(
             task.title,
             style: TextStyle(
@@ -23,8 +44,9 @@ class TaskListScreen extends StatelessWidget {
           subtitle: Text(task.description),
           trailing: Checkbox(
             value: task.isCompleted,
-            onChanged: (value) => onToggle(index, value),
+            onChanged: (value) => widget.onToggle(index, value),
           ),
+        ),
         );
       },
     );
