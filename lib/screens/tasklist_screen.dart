bde001c9d7cff55e17e765842dbc0d9b85ac1c76
Somye <mahajan.somye@gmail.com>
2024-12-17 20:29:49 +0530

    Merge

--- lib/screens/tasklist_screen.dart
+++ lib/screens/tasklist_screen.dart
@@ -8,9 +8,18 @@ class TaskListScreen extends StatefulWidget {
   final Function(int, bool?) onToggle;
   final Function(int) onEdit;
 
-  const TaskListScreen(
-      {super.key, required this.tasks, required this.onToggle});
+  const TaskListScreen({
+    super.key,
+    required this.tasks,
+    required this.onToggle,
+    required this.onEdit,
+  });
 
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -20,40 +29,44 @@ class TaskListScreen extends StatefulWidget {
       itemBuilder: (context, index) {
         final task = widget.tasks[index];
         return Slidable(
-          endActionPane: ActionPane(motion: StretchMotion(), children: [
-            SlidableAction(
-              onPressed: (context) async {
-                setState(() {
-                widget.tasks.removeAt(index);
-                });
-                await TaskStorage.saveTasks(widget.tasks);
-
-              },
-              icon: Icons.delete,
-              foregroundColor: Colors.red,
-            ),
-          ]),
-          child: ListTile(
-          title: Text(
-            task.title,
-            style: TextStyle(
-              decoration: task.isCompleted ? TextDecoration.lineThrough : null,
-            ),
-          ),
-          subtitle: Text(task.description),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
             children: [
-              IconButton(
-                onPressed: () => onEdit(index),
-                icon: const Icon(Icons.edit),
-              ),
-              const SizedBox(width: 5),
-              Checkbox(
-                value: task.isCompleted,
-                onChanged: (value) => onToggle(index, value),
+              SlidableAction(
+                onPressed: (context) async {
+                  setState(() {
+                    widget.tasks.removeAt(index);
+                  });
+                  await TaskStorage.saveTasks(widget.tasks);
+                },
+                icon: Icons.delete,
+                foregroundColor: Colors.red,
               ),
             ],
+          ),
+          child: ListTile(
+            title: Text(
+              task.title,
+              style: TextStyle(
+                decoration:
+                    task.isCompleted ? TextDecoration.lineThrough : null,
+              ),
+            ),
+            subtitle: Text(task.description),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  onPressed: () => widget.onEdit(index),
+                  icon: const Icon(Icons.edit),
+                ),
+                const SizedBox(width: 5),
+                Checkbox(
+                  value: task.isCompleted,
+                  onChanged: (value) => widget.onToggle(index, value),
+                ),
+              ],
+            ),
           ),
         );
       },
