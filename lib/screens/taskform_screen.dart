cc5bfcf9ac9416baf92e573ec41cc00f1d10d693
sanat <sanatjha4@gmail.com>
2024-12-19 22:57:52 +0530

    Overflow solved

--- lib/screens/taskform_screen.dart
+++ lib/screens/taskform_screen.dart
@@ -20,49 +20,51 @@ class _TaskFormScreenState extends State<TaskFormScreen> {
       appBar: AppBar(
         title: const Text('Add Task'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _key,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(labelText: 'Task Title'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Title cannot be empty!";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _descController,
-                decoration:
-                    const InputDecoration(labelText: 'Task Description'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Description cannot be empty!";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_key.currentState!.validate()) {
-                    Task task = Task(
-                      title: _titleController.text,
-                      description: _descController.text,
-                    );
-                    Fluttertoast.showToast(msg: "Task Successfully Created!");
-                    Navigator.pop(context,task);
-                  }
-                },
-                child: const Text('Save Task'),
-              ),
-            ],
+      body: SigleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _key,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(labelText: 'Task Title'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Title cannot be empty!";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _descController,
+                  decoration:
+                      const InputDecoration(labelText: 'Task Description'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Description cannot be empty!";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_key.currentState!.validate()) {
+                      Task task = Task(
+                        title: _titleController.text,
+                        description: _descController.text,
+                      );
+                      Fluttertoast.showToast(msg: "Task Successfully Created!");
+                      Navigator.pop(context,task);
+                    }
+                  },
+                  child: const Text('Save Task'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
