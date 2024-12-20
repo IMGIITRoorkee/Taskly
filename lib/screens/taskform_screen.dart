2d4cab7ef516d2580f7e76a7298d1f6ac660a54e
sanat <sanatjha4@gmail.com>
2024-12-20 23:04:36 +0530

    Added Task dependency feature #28

--- lib/screens/taskform_screen.dart
+++ lib/screens/taskform_screen.dart
@@ -45,99 +45,97 @@ class _TaskFormScreenState extends State<TaskFormScreen> {
         child: Form(
           key: _key,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _titleController,
-                  decoration: const InputDecoration(labelText: 'Task Title'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Title cannot be empty!";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _descController,
-                  decoration:
-                      const InputDecoration(labelText: 'Task Description'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Description cannot be empty!";
-                    }
-                    return null;
-                  },
-                ),
-
-                // Dropdown for selecting a dependency task
-                DropdownButtonFormField<String?>(
-                  value: selectedDependency?.title, // Compare by title
-                  items: [
-                    const DropdownMenuItem<String?>(
-                      value: null,
-                      child: Text('None'),
-                    ),
-                    ...widget.availableTasks.map((task) {
-                      return DropdownMenuItem<String?>(
-                        value: task.title, // Use title as the value
-                        child: Text(task.title),
-                      );
-                    }).toList(),
-                  ],
-                  onChanged: (String? newTaskTitle) {
-                    setState(() {
-                      selectedDependency = widget.availableTasks.firstWhere(
-                        (task) => task.title == newTaskTitle,
-                        orElse: () => null as Task,
-                      );
-                    });
-                  },
-                  decoration: const InputDecoration(labelText: 'Dependency'),
-                ),
-
-                // Date picker field for a DateTime value (deadline)
-                ElevatedButton(
-                  onPressed: () async {
-                    final selectedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime(2025),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Task Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Title cannot be empty!";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _descController,
+                decoration:
+                    const InputDecoration(labelText: 'Task Description'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Description cannot be empty!";
+                  }
+                  return null;
+                },
+              ),
+          
+              // Dropdown for selecting a dependency task
+              DropdownButtonFormField<String?>(
+                value: selectedDependency?.title, // Compare by title
+                items: [
+                  const DropdownMenuItem<String?>(
+                    value: null,
+                    child: Text('None'),
+                  ),
+                  ...widget.availableTasks.map((task) {
+                    return DropdownMenuItem<String?>(
+                      value: task.title, // Use title as the value
+                      child: Text(task.title),
                     );
-                    if (selectedDate != null) {
-                      setState(() {
-                        hasDeadline = true;
-                        deadline = selectedDate;
-                      });
-                    }
-                  },
-                  child: const Text('Select Deadline'),
-                ),
-
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_key.currentState!.validate()) {
-                      Task task = Task(
-                        title: _titleController.text,
-                        description: _descController.text,
-                        hasDeadline: hasDeadline,
-                        deadline: hasDeadline ? deadline : null,
-                        dependency:
-                            selectedDependency, // Set the selected dependency
-                      );
-                      Fluttertoast.showToast(
-                          msg: editing
-                              ? "Task Successfully Edited!"
-                              : "Task Successfully Created!");
-                      Navigator.pop(context, task);
-                    }
-                  },
-                  child: const Text('Save Task'),
-                ),
-              ],
-            ),
+                  }).toList(),
+                ],
+                onChanged: (String? newTaskTitle) {
+                  setState(() {
+                    selectedDependency = widget.availableTasks.firstWhere(
+                      (task) => task.title == newTaskTitle,
+                      orElse: () => null as Task,
+                    );
+                  });
+                },
+                decoration: const InputDecoration(labelText: 'Dependency'),
+              ),
+          
+              // Date picker field for a DateTime value (deadline)
+              ElevatedButton(
+                onPressed: () async {
+                  final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2025),
+                  );
+                  if (selectedDate != null) {
+                    setState(() {
+                      hasDeadline = true;
+                      deadline = selectedDate;
+                    });
+                  }
+                },
+                child: const Text('Select Deadline'),
+              ),
+          
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_key.currentState!.validate()) {
+                    Task task = Task(
+                      title: _titleController.text,
+                      description: _descController.text,
+                      hasDeadline: hasDeadline,
+                      deadline: hasDeadline ? deadline : null,
+                      dependency:
+                          selectedDependency, // Set the selected dependency
+                    );
+                    Fluttertoast.showToast(
+                        msg: editing
+                            ? "Task Successfully Edited!"
+                            : "Task Successfully Created!");
+                    Navigator.pop(context, task);
+                  }
+                },
+                child: const Text('Save Task'),
+              ),
+            ],
           ),
         ),
       ),
