3b7b3c18564f70310dfab6a2e66aed4e92622ebf
Somye <mahajan.somye@gmail.com>
2024-12-28 17:48:47 +0530

    Updated task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -4,9 +4,19 @@ class Task {
   bool isCompleted;
   DateTime deadline;
   bool hasDeadline;
+  bool isRecurring;
+  int recurringDays;
 
-  Task({required this.title, this.description = '', this.isCompleted = false, DateTime? deadline, this.hasDeadline = false})
-      : deadline = deadline ?? DateTime.now();
+  Task(
+      {required this.title,
+      this.description = '',
+      this.isCompleted = false,
+      DateTime? deadline,
+      this.hasDeadline = false,
+      this.isRecurring = false,
+      int? recurringDays})
+      : deadline = deadline ?? DateTime.now(),
+        recurringDays = recurringDays ?? 0;
 
   // Convert a Task object to JSON
   Map<String, dynamic> toJson() {
@@ -16,6 +26,8 @@ class Task {
       'isCompleted': isCompleted,
       'deadline': deadline.toIso8601String(),
       'hasDeadline': hasDeadline,
+      'isRecurring': isRecurring,
+      'recurringDays': recurringDays,
     };
   }
 
@@ -27,6 +39,8 @@ class Task {
       isCompleted: json['isCompleted'],
       deadline: DateTime.parse(json['deadline']),
       hasDeadline: json['hasDeadline'],
+      isRecurring: json['isRecurring'],
+      recurringDays: json['recurringDays'],
     );
   }
 }
