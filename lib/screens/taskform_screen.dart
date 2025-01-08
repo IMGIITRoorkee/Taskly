ff744e95f4c6f1010f889d2b490268a94a429ab9
sanat <sanatjha4@gmail.com>
2025-01-08 18:50:19 +0530

    deadline late date bug fixed

--- lib/screens/taskform_screen.dart
+++ lib/screens/taskform_screen.dart
@@ -203,7 +203,7 @@ class _TaskFormScreenState extends State<TaskFormScreen> {
               context: context,
               initialDate: DateTime.now(),
               firstDate: DateTime.now(),
-              lastDate: DateTime(2025),
+              lastDate: DateTime.now().add(const Duration(days: 365)),
             );
             if (selectedDate != null) {
               setState(() {
