677c7c2b9578a0c9816396fcd0aa94ddf79d6f1c
Somye <mahajan.somye@gmail.com>
2025-01-10 07:37:10 +0530

    Added models

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskly/models/subtask.dart';
 
 class Task {
   String id;
@@ -9,21 +10,24 @@ class Task {
   Color color;
   int? recurringDays;
   Task? dependency;
+  List<Subtask> subtasks;
 
   bool get isRecurring => recurringDays != null;
 
   bool get hasDeadline => deadline != null;
 
-  Task(
-      {required this.title,
-      this.description = '',
-      this.isCompleted = false,
-      this.deadline,
-      this.color = Colors.blue,
-      this.recurringDays,
-      this.dependency,
-      String? id})
-      : id = id ?? DateTime.now().microsecondsSinceEpoch.toString();
+  Task({
+    required this.title,
+    this.description = '',
+    this.isCompleted = false,
+    this.deadline,
+    this.color = Colors.blue,
+    this.recurringDays,
+    this.dependency,
+    String? id,
+    List<Subtask>? subtasks,
+  })  : id = id ?? DateTime.now().microsecondsSinceEpoch.toString(),
+        subtasks = [];
 
   // Convert a Task object to JSON
   Map<String, dynamic> toJson() {
@@ -36,11 +40,20 @@ class Task {
       'recurringDays': recurringDays,
       'color': color.value,
       'id': id,
+      'subtasks': subtasks.map((e) => e.toMap()).toList(),
     };
   }
 
   // Create a Task object from JSON
   factory Task.fromJson(Map<String, dynamic> json) {
+    List<Subtask> subtasks;
+    if (json['subtasks'] == null) {
+      subtasks = [];
+    } else {
+      subtasks =
+          (json['subtasks'] as List).map((e) => Subtask.fromMap(e)).toList();
+    }
+
     return Task(
       title: json['title'],
       description: json['description'],
@@ -51,6 +64,7 @@ class Task {
       recurringDays: json['recurringDays'],
       color: Color(json['color']),
       id: json['id'] ?? DateTime.now().millisecondsSinceEpoch.toString(),
+      subtasks: subtasks,
     );
   }
 
