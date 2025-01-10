1f7e4100a0f972e2a1f2c47f9e5c11e1fee8445c
Somye <mahajan.somye@gmail.com>
2025-01-10 16:47:54 +0530

    Added email service

---
+++ lib/service/email_service.dart
@@ -0,0 +1,32 @@
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+class EmailService {
+  static String username = 'somye.cool15@gmail.com';
+  static String password = 'keNfog-4zerqa-hytnov';
+
+  static void sendMessage() async {
+    final smtpServer = gmail(username, password);
+    // Use the SmtpServer class to configure an SMTP server:
+    // final smtpServer = SmtpServer('smtp.domain.com');
+    // See the named arguments of SmtpServer for further configuration
+    // options.
+
+    // Create our message.
+    final message = Message()
+      ..from = Address(username, 'Your name')
+      ..recipients.add('anyone.mahajan@gmail.com')
+      ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
+      ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+      ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
+
+    try {
+      final sendReport = await send(message, smtpServer);
+    } on MailerException catch (e) {
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
+    }
+  }
+}
